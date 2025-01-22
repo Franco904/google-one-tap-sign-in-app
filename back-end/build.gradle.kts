a5108ddc9b80694471fd160cbd5d99bf235ec524
Franco904 <francostavares2003@gmail.com>
2025-01-22 20:17:32 -0300

    fix build.gradle.kts dependencies

--- back-end/build.gradle.kts
+++ back-end/build.gradle.kts
@@ -1,22 +1,50 @@
 plugins {
-    kotlin("jvm") version "2.1.0"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlin.plugin.serialization)
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "com.example"
+version = "0.0.1"
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-}
+    // Core
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.host.common)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.config.yaml)
 
-tasks.test {
-    useJUnitPlatform()
-}
+    // Serialization
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
 
-//kotlin {
-//    jvmToolchain(17)
-//}
+    // Exception handling
+    implementation(libs.ktor.server.status.pages)
+
+    // Security
+    implementation(libs.ktor.server.auth)
+    implementation(libs.ktor.server.sessions)
+
+    // Monitoring
+    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.call.logging)
+
+    // Koin (DI)
+    implementation(libs.koin.ktor)
+    implementation(libs.koin.logger.slf4j)
+
+    // Testing
+    testImplementation(libs.kotlin.test.junit)
+    testImplementation(libs.ktor.server.test.host)
+}

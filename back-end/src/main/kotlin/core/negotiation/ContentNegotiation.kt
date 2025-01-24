897d1e79557c19a168afff6d666ba092b5726682
Franco904 <francostavares2003@gmail.com>
2025-01-24 07:44:34 -0300

    add json configurations in ContentNegotiation

--- back-end/src/main/kotlin/core/negotiation/ContentNegotiation.kt
+++ back-end/src/main/kotlin/core/negotiation/ContentNegotiation.kt
@@ -3,9 +3,15 @@ package com.example.core.negotiation
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
+import kotlinx.serialization.json.Json
 
 fun Application.configureContentNegotiation() {
     install(ContentNegotiation) {
-        json()
+        json(
+            Json {
+                prettyPrint = true
+                isLenient = true
+            }
+        )
     }
 }

50f8e54a1166357fbae7ac998296f8f894ed40af
Franco904 <francostavares2003@gmail.com>
2025-01-31 21:29:34 -0300

    increase session expiration time

--- back-end/src/main/kotlin/core/security/session/UserSession.kt
+++ back-end/src/main/kotlin/core/security/session/UserSession.kt
@@ -1,10 +1,11 @@
 package com.example.core.security.session
 
+import com.example.core.constants.SESSION_EXPIRATION_TIME_MILLIS
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class UserSession(
     val id: String,
     val name: String,
-    val expirationTimestamp: Long = System.currentTimeMillis() + 10000L,
+    val expirationTimestamp: Long = System.currentTimeMillis() + SESSION_EXPIRATION_TIME_MILLIS,
 )

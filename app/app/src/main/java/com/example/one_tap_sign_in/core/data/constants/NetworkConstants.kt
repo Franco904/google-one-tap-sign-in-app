3f0b276a2e0cd63a57da7ea17915ddbad21b7237
Franco904 <francostavares2003@gmail.com>
2025-04-20 17:05:07 -0300

    rename constant

--- app/app/src/main/java/com/example/one_tap_sign_in/core/data/constants/NetworkConstants.kt
+++ app/app/src/main/java/com/example/one_tap_sign_in/core/data/constants/NetworkConstants.kt
@@ -1,6 +1,6 @@
 package com.example.one_tap_sign_in.core.data.constants
 
-const val AUTH_BASE_URL = "http://192.168.15.25:8080"
+const val REMOTE_API_URL = "http://192.168.15.25:8080"
 
 const val CLIENT_ID = "400313030888-1snvsm2glcgp03b8gb4es4lofv1gv3ua.apps.googleusercontent.com"
 const val SESSION_COOKIE_NAME = "USER_SESSION"
